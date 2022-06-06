8918298d6e0e09fe2fecdaefcfbc013f7290328f
Saromase <seite.romain@hotmail.fr>
2022-06-06 15:13:35 +0200

    Verification of customer change

--- app/Http/Middleware/CustomerUrl.php
+++ app/Http/Middleware/CustomerUrl.php
@@ -6,6 +6,9 @@ use Closure;
 use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\URL;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\DB;
 
 class CustomerUrl
 {
@@ -18,12 +21,29 @@ class CustomerUrl
      */
     public function handle(Request $request, Closure $next)
     {
-        // dump( $request->route()->parameters()['customer']->id);
-        $customer = $request->route()->parameters()['customer'];
 
-        URL::defaults([
-            'customer' => 'athome',
-        ]);
+        $customer = $request->route()->parameters()['customer'];
+        $currentCustomer = $request->session()->get('customer');
+        
+        if ($customer) {
+            URL::defaults([
+                'customer' => $customer->name,
+            ]);
+        }
+
+        dump($currentCustomer, Config::get('database.connections.mysql'));
+        
+        
+        if ($currentCustomer === null) {
+            $request->session()->put('customer', $customer->name);
+            DB::disconnect();
+            Config::set('database.connections.mysql.database', $customer->name); 
+            DB::reconnect();
+
+        } elseif ($currentCustomer !== $customer->name) {
+            $request->session()->invalidate();
+            return redirect()->route(RouteServiceProvider::HOME);
+        }
 
         return $next($request);
     }
