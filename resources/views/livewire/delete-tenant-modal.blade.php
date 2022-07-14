01cc79e28f92c4def9d20e8ecbc3bf8109016178
Saromase <seite.romain@hotmail.fr>
2022-07-14 20:43:56 +0200

    Uppercase Tenant name

--- resources/views/livewire/delete-tenant-modal.blade.php
+++ resources/views/livewire/delete-tenant-modal.blade.php
@@ -8,7 +8,7 @@
                 <svg class="mx-auto mb-4 text-gray-400 w-14 h-14 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
                 <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
                     {{ Str::ucfirst(__('admin.tenants.alert_delete_tenant', [
-                        'name' => $tenant->name
+                        'name' => Str::upper($tenant->name)
                     ])) }}
                 </h3>
                 <form action="{{ route('tenants.destroy', $tenant) }}" method="POST">
